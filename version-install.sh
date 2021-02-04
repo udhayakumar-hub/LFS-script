16c26e9c764129f52f411833743f80656559bedc
udhayakumar <udhayakumar0405@outlook.com>
2021-02-04 22:56:31 +0530

    final

--- version-install.sh
+++ version-install.sh
@@ -2,6 +2,7 @@
 # Simple script to list version numbers of critical development tools
 echo "install-Basic_utility on LFS on ubuntu"
 echo "==^^please sceript RUN AS ROOT^^^====="
+
 sudo ln -sf bash /bin/sh 
 sudo apt-get install bison -y
 sudo apt-get install yacc -y 
@@ -10,6 +11,16 @@ sudo apt-get install m4 -y
 sudo apt-get install make -y
 sudo apt-get install patch -y
 sudo apt-get install texinfo -y
+sudo apt-get install bzip2 -y
+echo "-------gcc -6 install---------"
+sudo apt-get update && \
+sudo apt-get install build-essential software-properties-common -y && \
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+sudo apt-get update && \
+sudo apt-get install gcc-6 g++-6 -y && \
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && \
+gcc -v
+echo "Basic installation successfull"
 
 export LC_ALL=C
 bash --version | head -n1 | cut -d" " -f2-4
