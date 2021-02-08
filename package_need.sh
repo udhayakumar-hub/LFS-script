774ddf028ec5c071713ba0a485559aaa93566de1
udhayakumar <udhayakumar0405@outlook.com>
2021-02-08 14:49:21 +0530

    final-gcc-commit

--- package_need.sh
+++ package_need.sh
@@ -13,9 +13,9 @@ sudo apt-get install patch -y
 sudo apt-get install texinfo -y
 sudo apt-get install bzip2 -y
 echo "-------gcc -6 install---------"
-sudo apt-get update && \
-sudo apt-get install build-essential software-properties-common -y && \
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
-sudo apt-get update && \
-sudo apt-get install gcc-6 g++-6 -y && \
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && \
+sudo apt-get update
+sudo apt-get install build-essential software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt-get install gcc-6 g++-6 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 
